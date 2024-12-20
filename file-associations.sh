13bcdc5c5d8a702574b5212e558eb969605b10f9
Clément Grennerat <clement.grennerat@free.fr>
2024-12-20 22:18:42 +0100

    Adding app store apps

---
+++ file-associations.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+echo " -> Setting file associations for SublimeText..."
+
+# List of text-based file extensions to associate with Sublime Text
+extensions=("txt" "md" "html" "htm" "xml" "css" "js" "json" "c" "cpp" "h" "hpp" "hxx" "cxx" "cc" "java" "py" "rb" "pl" "sh" "bash" "ps1" "psm1" "f" "for" "f90" "f95" "f03" "f08" "s" "asm" "sln" "vcxproj" "cs" "vb" "fs" "fsi" "fsx" "idl" "odl" "php" "shtml" "php3" "php4" "php5" "php7" "phtml" "ctp" "twig" "jsp" "jspx" "cfm" "cfc" "dhtml" "xhtml" "jhtml" "erb" "rhtml" "htaccess" "htpasswd" "conf" "ini" "yaml" "yml" "toml" "toc" "mdown" "mkdn" "mdwn" "mdtxt" "mdtext" "textile" "rst" "rs" "properties" "stl" "sql" "csv" "tsv" "tab" "log" "cfg" "reg" "manifest" "bat" "cmd" "pem" "asc" "key" "pub" "xml.dist" "jsonld" "n3" "ttl" "trig" "rq" "sparql" "rq.in" "rq.out" "rq.result" "yaml-tmlanguage" "xsl" "xslt" "xul" "xbl" "xaml" "xsd" "wsdl" "bib" "en" "tex" "sty" "cls" "bbx" "cbx" "lbx" "m" "webmanifest" "iml" "swift" "plist" "mailsignature" " entitlements" "lst" "ir" "plist" "gitignore" "obj" )
+
+for extension in "${extensions[@]}"
+do
+    duti -s com.sublimetext.4 ".$extension" all
+done
---
+++ setup-all.sh
@@ -0,0 +1,40 @@
+#!/bin/zsh
+
+echo "✨ Installing and configuring everything on this mac..."
+
+# Mkdir directroy ~/Code
+echo "> Creating tree..."
+mkdir -p ~/Code
+
+CMD_DIR=$(pwd)
+
+# Installing Strap
+echo "> Using Strap to setup this mac..."
+git clone --depth 1 https://github.com/MikeMcQuaid/strap ~/Downloads/strap
+cd ~/Downloads/strap || exit
+bash strap.sh
+
+# Installing Brewfile
+echo "> Installing Software from the Brewfile..."
+cd ~/Downloads/ || exit
+brew bundle --file=~/Code/dotfiles/Brewfile
+
+# Installing App Store Apps
+echo "> Installing App Store Apps..."
+mas install 1276493162 # reMarkable Desktop
+mas install 1289583905 # Pixelmator Pro
+mas install 1548711022 # Barbee - Hide Menu Bar Items
+mas install 1287239339 # ColorSlurp
+mas install 1355679052 # Dropover - Easier Drag & Drop
+mas install 1085114709 # Parallels Desktop
+mas install 1043565969 # Screen Bandit
+mas install 570549457  # Spotica Menu
+mas install 6443470555 # Vivid - Double your brightness
+mas install 1502839586 # Hand Mirror
+mas install 1629008763 # Little Snitch Mini
+mas install 937984704  # Amphetamine
+
+# Setting up files associations
+echo "> Setting up file associations..."
+cd "$CMD_DIR" || exit
+./file-associations.sh
