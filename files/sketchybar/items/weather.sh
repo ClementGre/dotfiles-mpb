646c69e41128c5cffc66fb1d5033658213c1729c
Clément Grennerat <clement.grennerat@clgr.io>
2025-12-03 16:52:38 +0100

    Adding weather sketchybar widget

---
+++ files/sketchybar/items/weather.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sketchybar -m \
+              --add item        weather_icon q \
+              --set weather_icon icon.font="SF Pro:Medium:16" \
+                                icon.drawing=on \
+                                label.drawing=off \
+                                icon.padding_left=0 \
+                                icon.padding_right=5 \
+                                icon="🌤️" \
+                                update_freq=600 \
+                                click_script="open -n '/System/Applications/Weather.app/'" \
+              --add item        weather_label q \
+              --set weather_label label.font="SF Pro:Bold:12" \
+                                  icon.drawing=off \
+                                  update_freq=600 \
+                                  label.padding_right=4 \
+                                  script="$PLUGIN_DIR/weather.sh" \
+                                  click_script="open -n '/System/Applications/Weather.app/'"
+
+# Subscribe to relevant events
+sketchybar --subscribe weather_label system_woke
---
+++ files/sketchybar/plugins/weather.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Get weather stats from the shortcut
+WEATHER_DATA="$(shortcuts run "WeatherStats")"
+
+# Split by ", " delimiter
+IFS=', ' read -r NONE CONDITION TEMPERATURE <<< "$WEATHER_DATA"
+
+# Switch/case for weather conditions and corresponding icons
+case "$CONDITION" in
+  "Ensoleillé"|" Clair"|"Sunny")
+    ICON="☀️"
+    ;;
+  "Nuageux"|"Cloudy"|"Partiellement nuageux"|"Partly Cloudy")
+    ICON="☁️"
+    ;;
+  "Pluvieux"|"Rainy"|"Averses"|"Showers")
+    ICON="🌧️"
+    ;;
+  "Orageux"|"Stormy"|"Orages"|"Thunderstorms")
+    ICON="⛈️"
+    ;;
+  "Neige"|"Snowy"|"Neige")
+    ICON="❄️"
+    ;;
+  "Brouillard"|"Foggy"|"Fog")
+    ICON="🌫️"
+    ;;
+  "Venteux"|"Windy")
+    ICON="💨"
+    ;;
+  *)
+    ICON="🌤️"
+    ;;
+esac
+
+# Update the weather icon and label
+sketchybar --set "weather_icon" icon="$ICON"
+sketchybar --set "weather_label" label="$TEMPERATURE"
