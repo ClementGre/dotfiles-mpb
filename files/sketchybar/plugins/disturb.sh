d84fc73f58c2c4dab0b41c02fabbc4c9e357d6e3
Clément Grennerat <clement.grennerat@clgr.io>
2026-04-03 10:55:30 +0200

    Fix dnd for macOS 26

--- files/sketchybar/plugins/disturb.sh
+++ files/sketchybar/plugins/disturb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-ENABLED=$(defaults read com.apple.controlcenter "NSStatusItem Visible FocusModes")
+ENABLED=$(defaults read com.apple.controlcenter "NSStatusItem VisibleCC FocusModes")
 
 if [ "$ENABLED" -eq 1 ]; then
   sketchybar --set "$NAME" drawing=on
